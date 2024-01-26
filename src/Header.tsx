127e7388c598b9cc4fb668635cced05a9d735447
Iam-Ebuka <ebukagrizz@gmail.com>
2024-01-26 09:01:25 +0100

    working on the header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import logo from "./assets/images/logo.svg"
+import logo from "./assets/images/logo.svg";
+import menuClose from "./assets/images/icon-menu-close.svg";
+import menuOpen from "./assets/images/icon-menu.svg";
 
 const Header: React.FC = () => {
   return (
-    <div className='flex'>
+    <div className='flex justify-between items-center container h-[100px] p-[20px] mx-auto max-w-[1100px]'>
         <div>
             <img src={logo} alt='logo' />
         </div>
         <div>
-            <a href='#home'>Home</a>
-            <a href='#home'>New</a>
-            <a href='#home'>Popular</a>
-            <a href='#home'>Trending</a>
-            <a href='#home'>Categories</a>
+            <div className='lg:hidden'>
+                <img src={menuOpen} alt="Open the menu" />
+            </div>
+            <div className="hidden lg:flex lg:space-x-4">
+                <a href='#home'>Home</a>
+                <a href='#home'>New</a>
+                <a href='#home'>Popular</a>
+                <a href='#home'>Trending</a>
+                <a href='#home'>Categories</a>
+            </div>
         </div>
     </div>
   )
